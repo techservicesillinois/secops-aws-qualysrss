0582cf66be725e0f6620edfa602fa5f2d4d59152
MKletz <michael.kletz.27@gmail.com>
2021-08-04 11:09:17 -0500

    working

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,39 @@
-import urllib3, xmltodict
+import requests, xmltodict, json, os, datetime, boto3, dateutil.parser
 
-def post_to_splunk(item):
+qualys_url = 'https://status.qualys.com/history.rss'
+# This is used to populate metadata in the Splunk event without hard coding the values
+caller_identity = boto3.client('sts').get_caller_identity()
+
+def clean_item(item):
+    # The "description" field is just raw HTML that is available in human readable form via the "link" field so it's removed to make the events smaller
+    del item['description']
+
+    # The publish date is converted to ISO format to be consistent with Splunk's preferences
+    item['pubDate'] = dateutil.parser.parse(item['pubDate']).isoformat()
+
+    # Splunk gets a formatted timestamp for ingestion time
+    item['timestamp'] = datetime.datetime.now().isoformat()
+
     return item
 
-def get_rss_data(url='https://status.qualys.com/history.rss'):
-    response = urllib3.PoolManager().request('GET', url)
-    data = xmltodict.parse(response.data)
+def post_to_splunk(item):
+    headers = {'Authorization':f"Splunk {os.environ['HEC_TOKEN']}"}
+
+    payload = {
+        'host':caller_identity['Arn'],
+        'source':qualys_url,
+        'sourcetype':'hec:test',
+        'fields':{'Account':caller_identity['Account']},
+        'event':clean_item(item)
+    }
+
+    requests.post(os.environ['HEC_ENDPOINT'], data=json.dumps(payload), headers=headers)
+
+def get_rss_data(url=qualys_url):
+    response = requests.get(url)
+    data = xmltodict.parse(response.content)
     return data['rss']['channel']['item']
 
 def lambda_handler(event, context):
-    previous_item_title = 'place holder'
     for item in get_rss_data():
-        if item['title'] == previous_item_title:
-            break
-        else:
-            post_to_splunk(item)
+        post_to_splunk(item)
