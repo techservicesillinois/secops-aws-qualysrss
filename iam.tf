0c1e99143d36a4c953f9a489824f903e0f174fac
MKletz <michael.kletz.27@gmail.com>
2021-08-13 09:39:40 -0500

    tracking state with DynamoDB (#2)
    * add state tracking via dynamoDB
    
    * header fixes
    
    * readme
    
    * space
    
    * updated zip
    
    * removed query
    
    * fmt
    
    * function name

--- iam.tf
+++ iam.tf
@@ -11,6 +11,19 @@ data "aws_iam_policy_document" "trust" {
   }
 }
 
+data "aws_iam_policy_document" "dynamodb_access" {
+  statement {
+    actions   = ["dynamodb:GetItem", "dynamodb:PutItem"]
+    resources = [resource.aws_dynamodb_table.qualys_rss.arn]
+  }
+}
+
+resource "aws_iam_policy" "dynamodb_access" {
+  name        = "qualys_rss_dynamodb_rw"
+  description = "allows for reading and writing of the qualyss_rss table"
+  policy      = data.aws_iam_policy_document.dynamodb_access.json
+}
+
 resource "aws_iam_role" "default" {
   name_prefix = var.name
   description = "Role for Lambda function ${var.name}"
@@ -21,4 +34,9 @@ resource "aws_iam_role" "default" {
 resource "aws_iam_role_policy_attachment" "lambda" {
   role       = aws_iam_role.default.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_access" {
+  role       = aws_iam_role.default.name
+  policy_arn = aws_iam_policy.dynamodb_access.arn
 }
--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ locals {
   lambda_zip = "qualys_rss.zip"
 }
 
+resource "aws_dynamodb_table" "qualys_rss" {
+  name           = var.name
+  hash_key       = "guid"
+  write_capacity = 100
+  read_capacity  = 100
+  attribute {
+    name = "guid"
+    type = "S"
+  }
+}
+
 resource "aws_lambda_function" "default" {
   description      = "Forwards the Qualys outage feed to the Splunk HEC."
   function_name    = var.name
@@ -11,11 +22,13 @@ resource "aws_lambda_function" "default" {
   handler          = "lambda_function.lambda_handler"
   filename         = local.lambda_zip
   source_code_hash = filebase64sha256(local.lambda_zip)
+  timeout          = 300
   environment {
     variables = {
       HEC_ENDPOINT = var.hec_endpoint
       HEC_TOKEN    = var.hec_token
       QUALYS_URL   = var.qualys_url
+      TABLE_NAME   = resource.aws_dynamodb_table.qualys_rss.name
     }
   }
 }
